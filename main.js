ab1977cf3f7da36f05921c39a92b4b885be9a308
kmoon <1218moonlight@gmail.com>
2022-02-28 14:04:06 +0900

    메타데이터 수정

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as nav from './JS/Project/Nav.js'
 import * as lang from './JS/Module/Language.js'
 
-nav.InitMenu(document.querySelectorAll('nav .menus li:nth-child(-n+4)'));
+nav.InitMenu(document.querySelectorAll('nav .menus li'));
 nav.InitMenuIcon(document.querySelector('nav .menu_icon'), document.querySelector('nav .contents'));
 nav.InitLanguageMenu(document.querySelectorAll('nav .lang li:not(:nth-child(2))'));
